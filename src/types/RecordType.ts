f433320ade2a1e69fe83fdf0f300385a55bac53b
Renan Salustiano <renan.salustiano@ter.grupomarista.org.br>
2024-11-07 01:34:41 -0300

    Add Interface

---
+++ src/types/RecordType.ts
@@ -0,0 +1,9 @@
+export interface RecordType{
+    company?: string,
+    content?: string,
+    language?: string,
+    description?: string,
+    campaign?: string,
+    where?: string,
+    plannedDate?: string
+}
